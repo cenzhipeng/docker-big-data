1600f393f690affbb13d0a2cfea89ef0148e909f
cenzhipeng <cen-zhipeng@qq.com>
2020-09-15 02:48:16 +0800

    基本功能搭建

--- rm-docker-machine.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker-machine rm default -y
--- start-docker-machine.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker-machine create --driver virtualbox --virtualbox-boot2docker-url ./rancheros.iso --virtualbox-disk-size "40000" --virtualbox-share-folder "./:/mnt/sda1/work" default
-eval $(docker-machine env default)
-#docker-machine-ipconfig  static default
-#docker-machine ssh default "echo 'cd /mnt/sda1/work' >> /home/docker/.bash_profile"
